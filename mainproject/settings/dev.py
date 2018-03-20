61496518175d722ec35f1a761a00eec98d239e1f
Xiaochen Du <xiaochen.du@duke.edu>
2018-03-20 15:05:38 -0400

    commited

--- mainproject/settings/dev.py
+++ mainproject/settings/dev.py
@@ -17,23 +17,3 @@ try:
     from mainproject.settings.local import *
 except:
     pass
-
-
-# LOGGING = {
-#     'version': 1,
-#     'disable_existing_loggers': False,
-#     'handlers': {
-#         'file': {
-#             'level': 'DEBUG',
-#             'class': 'logging.FileHandler',
-#             'filename': 'debug.log',
-#         },
-#     },
-#     'loggers': {
-#         'django': {
-#             'handlers': ['file'],
-#             'level': 'DEBUG',
-#             'propagate': True,
-#         },
-#     },
-# }
