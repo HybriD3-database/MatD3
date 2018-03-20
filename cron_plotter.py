45add89a11b4dab6786b614578635d1221b2dc7f
Xiaochen Du <xiaochen.du@duke.edu>
2018-03-20 17:34:30 -0400

    updated plot scripts

--- cron_plotter.py
+++ cron_plotter.py
@@ -8,31 +8,27 @@ from materials.plotting.bs_plotting import prep_and_plot
 
 directory = os.getcwd()
 sys.path.append(directory)
-os.environ['DJANGO_SETTINGS_MODULE'] = 'mainproject.settings.dev'
 django.setup()
 
 from materials.models import ExcitonEmission, BandStructure
 
 # list out the location of all unplotted EE FILES
 emissions = ExcitonEmission.objects.filter(plotted=False)
-entries = [str(e.pl_file).split("/")[1] for e in emissions]
 
-count = 0
-while count < len(emissions):
-    pl_file_loc = MEDIA_ROOT + "/uploads/" + entries[count]
-    plotpl(pl_file_loc)
-    emissions[count].plotted = True
-    emissions[count].save()
-    print("{} plotted and saved".format(emissions[count]))
-    count += 1
+for emission in emissions:
+    if emission.pl_file:
+        pl_file_loc = MEDIA_ROOT + "/uploads/" + str(emission.pl_file).split("/")[1]
+        plotpl(pl_file_loc)
+        emission.plotted = True
+        emission.save()
+        print("{} plotted and saved".format(str(emission)))
 
 band_structures = BandStructure.objects.filter(plotted=False)
-entries = [str(bs.folder_location) for bs in band_structures]
 
-count = 0
-while count < len(band_structures):
-    prep_and_plot(entries[0])
-    band_structures[count].plotted = True
-    band_structures[count].save()
-    print("{} plotted and saved".format(band_structures[count]))
-    count += 1
+for band in band_structures:
+    folder_loc = band.folder_location
+    if os.path.isdir(folder_loc):
+        prep_and_plot(folder_loc)
+        band.plotted = True
+        band.save()
+        print("{} plotted and saved".format(folder_loc))
---
+++ cron_plotter_dev.py
@@ -0,0 +1,4 @@
+import os
+os.environ['DJANGO_SETTINGS_MODULE'] = 'mainproject.settings.dev'
+
+import cron_plotter
