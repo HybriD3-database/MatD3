a72ac6b274ec909cc860a3cbb51a7eee747ad535
Raul Laasner <raullaasner@gmail.com>
2019-02-18 22:35:17 -0500

    Update requirements

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,8 @@
-from django.contrib.auth.views import logout
-from django.contrib.auth.views import password_reset
-from django.contrib.auth.views import password_reset_complete
-from django.contrib.auth.views import password_reset_confirm
-from django.contrib.auth.views import password_reset_done
+from django.contrib.auth.views import LogoutView
+from django.contrib.auth.views import PasswordResetView
+from django.contrib.auth.views import PasswordResetCompleteView
+from django.contrib.auth.views import PasswordResetConfirmView
+from django.contrib.auth.views import PasswordResetDoneView
 from django.urls import path
 from django.urls import re_path
 
@@ -13,8 +13,8 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('login/', views.login, {'template_name': 'accounts/login.html'},
          name='login'),
-    path('logout/', logout, {'template_name': 'accounts/logout.html'},
-         name='logout'),
+    path('logout/', LogoutView.as_view(),
+         {'template_name': 'accounts/logout.html'}, name='logout'),
     path('register/', views.register, name='register'),
     re_path(r'^activate/(?P<uidb64>[0-9A-Za-z_\-]+)/'
             r'(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
@@ -24,20 +24,20 @@ urlpatterns = [
             name='view_profile_with_pk'),
     path('profile/edit/', views.edit_profile, name='edit_profile'),
     path('change-password/', views.change_password, name='change_password'),
-    path('reset-password/', password_reset,
+    path('reset-password/', PasswordResetView.as_view(),
          {'template_name': 'accounts/reset_password.html',
           'post_reset_redirect': 'accounts:password_reset_done',
           'email_template_name': 'accounts/reset_password_email.html'},
          name='reset_password'),
-    path('reset-password/done', password_reset_done,
+    path('reset-password/done', PasswordResetDoneView.as_view(),
          {'template_name': 'accounts/reset_password_done.html'},
          name='password_reset_done'),
     re_path(r'^reset-password/confirm(?P<uidb64>[0-9A-Za-z]+)-(?P<token>.+)/$',
-            password_reset_confirm,
+            PasswordResetConfirmView.as_view(),
             {'template_name': 'accounts/reset_password_confirm.html',
              'post_reset_redirect': 'accounts:password_reset_complete'},
             name='password_reset_confirm'),
-    path('reset-password/complete/', password_reset_complete,
+    path('reset-password/complete/', PasswordResetCompleteView.as_view(),
          {'template_name': 'accounts/reset_password_complete.html'},
          name='password_reset_complete')
 ]
