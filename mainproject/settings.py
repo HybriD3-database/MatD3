ff1a15368e507d8d6f1e365d7c9bf0de97b1436a
Raul Laasner <raullaasner@gmail.com>
2020-06-26 10:22:00 +0200

    Read EMAIL_USE_TLS from the correct variable
    Hopefully this one fixes c3809b66.

--- mainproject/settings.py
+++ mainproject/settings.py
@@ -160,7 +160,7 @@ EMAIL_HOST = config('EMAIL_HOST', default='')
 EMAIL_PORT = config('EMAIL_PORT', default=587, cast=int)
 EMAIL_HOST_USER = config('EMAIL_HOST_USER', default='')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD', default='')
-EMAIL_USE_TLS = config('EMAIL_PORT', default=True, cast=bool)
+EMAIL_USE_TLS = config('EMAIL_USE_TLS', default=True, cast=bool)
 
 # Messages framework
 
